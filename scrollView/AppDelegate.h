d8493c10ca951b090532af66377b0aebcbfe55a6
MonkKou <MonkKou@MonkKoutekiMacBook-Pro.local>
2012-10-23 10:12:16 +0800

    Initial Commit

---
+++ scrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scrollView
+//
+//  Created by MonkKou on 12-10-23.
+//  Copyright (c) 2012年 MonkKou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ scrollView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  scrollView
+//
+//  Created by MonkKou on 12-10-23.
+//  Copyright (c) 2012年 MonkKou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
